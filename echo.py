5f178585d3db51398e005b738aa3e2423402881b
Mark Buchmoyer <buchmoyerm@gmail.com>
2018-02-28 21:56:39 -0500

    initial echo

---
+++ echo.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/echo')
+def echo():
+    return 'you said: {}'.format(request.args.get('text',''))
+
+if __name__ == '__main__':
+    app.run()
